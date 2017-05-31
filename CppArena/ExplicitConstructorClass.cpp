ac2dc29625cbebcd001d13ca66005f8d4e43eeda
Olivia <olivia_git@outlook.com>
2017-05-31 21:41:11 +0200

    Adding ExplicitConstructorClass

---
+++ CppArena/ExplicitConstructorClass.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "ExplicitConstructorClass.h"
+
+ExplicitConstructorClass::ExplicitConstructorClass(int value)
+{
+}
+
+void CreateExplicitConstructorClass()
+{
+	int x = 1;
+	ExplicitConstructorClass c1(x);
+
+	// compiles, but gives warning
+	//double y = 1.0;
+	//ExplicitConstructorClass c2(y);
+}
---
+++ CppArena/ExplicitConstructorClass.h
@@ -0,0 +1,8 @@
+#pragma once
+
+ref class ExplicitConstructorClass
+{
+public:
+	ExplicitConstructorClass(int value);
+};
+
